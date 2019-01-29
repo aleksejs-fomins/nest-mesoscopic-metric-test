5b0c3c6c6f967fd01b400e1c40d2f86a4cb613c0
Aleksejs Fomins <aleksejs.fomins@uzh.ch>
2019-01-29 18:17:53 +0100

    crosscorr

---
+++ codes/lib-unitttest/test_crosscorr.py
@@ -0,0 +1,89 @@
+# Export library path
+import os, sys
+thispath = os.path.dirname(os.path.abspath(__file__))
+p1path = os.path.abspath(os.path.join(thispath, os.pardir))
+p2path = os.path.abspath(os.path.join(p1path, os.pardir))
+sys.path.append(os.path.join(p1path, 'lib/'))
+
+# import standard libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# import special libraries
+from corr_lib import crossCorr
+
+def plotRez(corrMat, corrDelMat, sprMat, sprDelMat, title):
+    fig, ax = plt.subplots(nrows=2, ncols=2)
+    fig.suptitle(title)
+    pl00 = ax[0][0].imshow(corrMat)
+    pl10 = ax[1][0].imshow(sprMat)
+    pl01 = ax[0][1].imshow(corrDelMat)
+    pl11 = ax[1][1].imshow(sprDelMat)
+
+    ax[0][0].set_title('Corr')
+    ax[1][0].set_title('Spr')
+    ax[0][1].set_title('CorrDel')
+    ax[1][1].set_title('SprDel')
+
+    fig.colorbar(pl00, ax=ax[0][0])
+    fig.colorbar(pl01, ax=ax[0][1])
+    fig.colorbar(pl10, ax=ax[1][0])
+    fig.colorbar(pl11, ax=ax[1][1])
+
+    plt.show()
+
+'''
+   Test 1:
+     Generate random data, and shift it by fixed steps for each channel
+     Expected outcomes:
+     * If shift <= max_delay, corr ~ 1, delay = shift
+     * If shift > max_delay, corr ~ 0, delay = rand
+     * Delay is the same for all diagonals, because we compare essentially the same data, both cycled by the same amount
+'''
+    
+N_NODE = 5
+N_DATA = 1000
+DELAY_MIN = 1
+DELAY_MAX = 2
+
+#data = np.random.uniform(0, 1, N_NODE*N_DATA).reshape((N_NODE, N_DATA))
+# Generate progressively more random data
+data = np.zeros((N_NODE, N_DATA))
+data[0] = np.random.normal(0, 1, N_DATA)
+for i in range(1, N_NODE):
+    data[i] = np.hstack((data[i-1][1:], data[i-1][0]))
+
+corrMat, corrDelMat = crossCorr(data, DELAY_MIN, DELAY_MAX, est='corr')
+sprMat, sprDelMat = crossCorr(data, DELAY_MIN, DELAY_MAX, est='spr')
+
+plotRez(corrMat, corrDelMat, sprMat, sprDelMat, 'Test 1: Channels are shifts of the same data')
+
+
+'''
+   Test 2:
+     Generate random data, all copies of each other, each following one a bit more noisy than prev
+     Expected outcomes:
+     * If shift <= max_delay, corr ~ 1, delay = shift
+     * If shift > max_delay, corr ~ 0, delay = rand
+     * Delay is the same for all diagonals, because we compare essentially the same data, both cycled by the same amount
+'''
+
+N_NODE = 5
+N_DATA = 1000
+DELAY_MIN = 0
+DELAY_MAX = 0
+alpha = 0.2
+
+data = np.random.normal(0, 1, N_NODE*N_DATA).reshape((N_NODE, N_DATA))
+for i in range(1, N_NODE):
+    data[i] = data[i-1]*(1 - alpha**2) + np.random.normal(0, 1, N_DATA) * (alpha**2)
+    
+print(np.var(data, axis=1))
+
+corrMat, corrDelMat = crossCorr(data, DELAY_MIN, DELAY_MAX, est='corr')
+sprMat, sprDelMat = crossCorr(data, DELAY_MIN, DELAY_MAX, est='spr')
+
+plotRez(corrMat, corrDelMat, sprMat, sprDelMat, 'Test 2: Channels are same, but progressively more noisy')
+
+
+
---
+++ codes/lib/corr_lib.py
@@ -0,0 +1,71 @@
+import numpy as np
+import scipy.stats
+
+
+# Spearmann rank matrix
+# Data - 2D matrix [channel x time]
+def sprMat(data):
+    N = data.shape[0]
+    rezMat = np.diag(np.ones(N))
+
+    for i in range(N):
+        for j in range(i+1, N):
+            spr = scipy.stats.spearmanr(data[i], data[j])[0]
+            rezMat[i][j] = spr
+            rezMat[j][i] = spr
+    return rezMat
+
+
+# Compute cross-correlation for all shifts in [delay_min, delay_max]
+# For each pair return largest corr by absolute value, and shift value
+# * Data - 2D matrix [channel x time]
+# * Data - 3D matrix [trial x max_delay+1 x channel]
+#   For fixed delay, trial and channel pair:
+#    - shift time by delay and get paired overlap
+#    - concatenate overlaps for all trials
+#    - compute corr over concatenation
+def crossCorr(data, delayMin, delayMax, est='corr'):
+    if len(data.shape) == 2:
+        nNode, nTime = data.shape
+        haveTrials = False
+    elif len(data.shape) == 3:
+        nTrial, nTime, nNode = data.shape
+        haveTrials = True
+    else:
+        raise ValueError('unexpected data shape', data.shape)
+        
+    # Check that number of timesteps is sufficient to estimate delayMax
+    if nTime <= delayMax:
+        raise ValueError('max delay', delayMax, 'cannot be estimated for number of timesteps', nTime)
+        
+    corrMat = np.zeros((nNode, nNode))
+    delayMat = np.zeros((nNode, nNode))
+    
+    for i in range(nNode):
+        for j in range(nNode):
+            for delay in range(delayMin, delayMax+1):
+                # Based on data type, select lagged variables to cross-correlate
+                l_sh = delay
+                r_sh = -delay if delay > 0 else nTime
+                
+                if haveTrials:
+                    x = data[:, :r_sh, i].flatten()
+                    y = data[:, l_sh:, j].flatten()
+                else:
+                    x = data[i][:r_sh]
+                    y = data[j][l_sh:]
+                
+                # Choose between Correlation and Spearman Rank estimators
+                if est == 'corr':
+                    corrThis = np.corrcoef(x, y)[0, 1]
+                elif est == 'spr':
+                    corrThis = scipy.stats.spearmanr(x, y)[0]
+                else:
+                    raise ValueError('unexpected estimator type', est)
+                    
+                # Keep estimate and delay if it has largest absolute value so far
+                if np.abs(corrThis) > np.abs(corrMat[i][j]):
+                    corrMat[i][j] = corrThis
+                    delayMat[i][j] = delay
+                        
+    return corrMat, delayMat
